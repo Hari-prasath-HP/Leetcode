a9482256c7ce17cf26025f00c5d2edecf8a6b06c
Hari-prasath-HP <hariprasath.bc004@gmail.com>
2025-01-07 10:34:48 +0530

    Create 1903.js

---
+++ 1903.js
@@ -0,0 +1,13 @@
+let num = "486379402";
+var largestOddNumber = function(num) {
+    if(num[num.length-1]%2==1){
+        return num
+    }
+    for(let i=num.length;i>=0;i--){
+        if(num[i]%2===1){
+            return num.substring(0,i+1)
+        }
+    }
+    return ""
+};
+console.log(largestOddNumber(num))
