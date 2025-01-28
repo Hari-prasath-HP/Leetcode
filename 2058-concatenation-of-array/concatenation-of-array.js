147781d50a0fa7fdccda670e45c745a76b2dfd63
Hari-prasath-HP <hariprasath.bc004@gmail.com>
2025-01-28 09:28:22 +0530

    Time: 2 ms (30.47%) | Memory: 54 MB (12.48%) - LeetSync

--- 2058-concatenation-of-array/concatenation-of-array.js
+++ 2058-concatenation-of-array/concatenation-of-array.js
@@ -3,5 +3,9 @@
  * @return {number[]}
  */
 var getConcatenation = function(nums) {
-    return [...nums,...nums]
+    let res = []
+    for(let i=0;i<nums.length;i++){
+        res[i] = nums [i]
+        res[i+nums.length] = nums[i]
+    }return res
 };
