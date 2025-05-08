fe9f62df5b04f3ffc044cec31b296233435b947c
Hari-prasath-HP <hariprasath.bc004@gmail.com>
2025-05-08 19:05:39 +0530

    Time: 54 ms (24.19%) | Memory: 54.4 MB (35.48%) - LeetSync

---
+++ 3617-find-the-original-typed-string-i/find-the-original-typed-string-i.js
@@ -0,0 +1,12 @@
+/**
+ * @param {string} word
+ * @return {number}
+ */
+var possibleStringCount = function(word) {
+    let count = 1
+for(let i=0;i<word.length-1;i++){
+    if(word[i] == word[i+1]){
+        count++
+    }
+}return count
+};
