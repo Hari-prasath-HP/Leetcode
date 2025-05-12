7001cf186eca9fe17b7eff684284b9ce9ace635d
Hari-prasath-HP <hariprasath.bc004@gmail.com>
2025-05-12 09:38:27 +0530

    Time: 0 ms (100.00%) | Memory: 58.8 MB (78.55%) - LeetSync

---
+++ 645-set-mismatch/set-mismatch.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var findErrorNums = function(nums) {
+    let first = 0;
+let second = 0
+let count = new Array(nums.length+1).fill(0)
+
+for(let i=0;i<nums.length;i++){
+    count[nums[i]] += 1
+}
+
+for(let j=1;j<count.length;j++){
+    if(count[j]>1){
+        first = j
+    }
+    if(count[j] == 0){
+        second = j
+    }
+}return [first,second]
+};
