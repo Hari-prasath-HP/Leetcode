53c44ac9a575dfb2c3b1b8f9f2ddd0e9d6c87d82
Hari-prasath-HP <hariprasath.bc004@gmail.com>
2025-06-18 10:21:45 +0530

    Time: 3 ms (63.52%) | Memory: 65.7 MB (15.77%) - LeetSync

---
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function(prices) {
+let min = prices[0]
+let profit = 0
+for(let i=1;i<prices.length;i++){
+    min = Math.min(prices[i],min)
+    profit = Math.max(prices[i]-min,profit)
+}
+return (profit)
+};
