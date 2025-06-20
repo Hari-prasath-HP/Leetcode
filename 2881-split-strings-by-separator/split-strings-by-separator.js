607807d0b5d8c07596231547cec3585d17c5795c
Hari-prasath-HP <hariprasath.bc004@gmail.com>
2025-06-20 11:52:37 +0530

    Time: 9 ms (35.26%) | Memory: 63.5 MB (18.95%) - LeetSync

---
+++ 2881-split-strings-by-separator/split-strings-by-separator.js
@@ -0,0 +1,11 @@
+/**
+ * @param {string[]} words
+ * @param {character} separator
+ * @return {string[]}
+ */
+var splitWordsBySeparator = function(words, separator) {
+    let arr = []
+    for(let char of words){
+        arr.push(...char.split(separator))
+    }return arr.filter((a)=>a!="")
+};
