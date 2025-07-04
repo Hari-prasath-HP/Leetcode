3d0bea8cf37ec05a0fab0d79cadbca769351afb9
Hari-prasath-HP <hariprasath.bc004@gmail.com>
2025-07-04 09:35:06 +0530

    Time: 0 ms (100.00%) | Memory: 53.9 MB (14.50%) - LeetSync

--- 434-number-of-segments-in-a-string/number-of-segments-in-a-string.js
+++ 434-number-of-segments-in-a-string/number-of-segments-in-a-string.js
@@ -4,7 +4,6 @@
  */
 var countSegments = function(s) {
 let res = 0
-let arr = s.split(' ')
-let arr1 = arr.filter((a)=> a!=='')
-return arr1.length
+let arr = s.split(' ').filter((a)=> a!=='')
+return arr.length
 };
