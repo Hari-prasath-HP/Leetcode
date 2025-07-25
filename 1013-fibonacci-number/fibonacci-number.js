73f22ef719107e35b956e018a8a26e208f38e4ad
Hari-prasath-HP <hariprasath.bc004@gmail.com>
2025-07-25 09:48:46 +0530

    Time: 29 ms (98.70%) | Memory: 53.5 MB (54.61%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.js
+++ 1013-fibonacci-number/fibonacci-number.js
@@ -3,7 +3,17 @@
  * @return {number}
  */
 var fib = function(n) {
-    if(n==0)return 0;
-    if(n==1)return 1
-    return fib(n-1)+fib(n-2)
+let a = 0,b=1,c=0
+if(n===0){
+    c = 0
+}else if(n===1){
+    c = 1
+}else{
+    for(i=0;i<n-1;i++){
+        c = a+b
+        a=b
+        b=c
+    }
+}
+return c
 };
