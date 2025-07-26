4dd14a04f5edb8c9105eef06b675336e8aae9b7e
Hari-prasath-HP <hariprasath.bc004@gmail.com>
2025-07-26 09:17:14 +0530

    Time: 3 ms (66.23%) | Memory: 58.2 MB (16.88%) - LeetSync

--- 3226-minimum-number-game/minimum-number-game.js
+++ 3226-minimum-number-game/minimum-number-game.js
@@ -3,11 +3,11 @@
  * @return {number[]}
  */
 var numberGame = function(nums) {
-    let sort = nums.sort((a,b)=>a-b)
+    nums.sort((a,b)=>a-b)
 let arr = []
 for(let i=0;i<nums.length;i+=2){
-    let minAlice = sort[i]
-    let minBob = sort[i+1]
+    let minAlice = nums[i]
+    let minBob = nums[i+1]
     arr.push(minBob)
     arr.push(minAlice)
 }return arr
