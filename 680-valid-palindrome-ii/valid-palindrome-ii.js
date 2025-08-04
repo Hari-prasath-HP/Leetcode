4c01f1d0479698eaafe4d11415bb91c036935b00
Hari-prasath-HP <hariprasath.bc004@gmail.com>
2025-08-04 09:52:44 +0530

    Time: 6 ms (75.29%) | Memory: 61.4 MB (27.14%) - LeetSync

--- 680-valid-palindrome-ii/valid-palindrome-ii.js
+++ 680-valid-palindrome-ii/valid-palindrome-ii.js
@@ -3,27 +3,21 @@
  * @return {boolean}
  */
 var validPalindrome = function(s) {
-    function isPalindrome(str, left, right) {
-        while (left < right) {
-            if (str[left] !== str[right]) {
-                return false;
-            }
-            left++;
-            right--;
-        }
-        return true;
+    function isPalindrome(str,left,right){
+    while(left < right){
+        if(str[left] !== str[right]){
+        return false
+    }left++
+    right--
     }
-
-    let left = 0;
-    let right = s.length - 1;
-
-    while (left < right) {
-        if (s[left] !== s[right]) {
-            return isPalindrome(s, left + 1, right) || isPalindrome(s, left, right - 1);
-        }
-        left++;
-        right--;
-    }
-
-    return true;
+    return true
+}
+let left = 0
+let right = s.length-1
+while(left < right){
+    if(s[left] !== s[right]){
+        return isPalindrome(s,left+1,right) || isPalindrome(s,left,right-1)
+    }left++
+    right--
+}return true
 };
