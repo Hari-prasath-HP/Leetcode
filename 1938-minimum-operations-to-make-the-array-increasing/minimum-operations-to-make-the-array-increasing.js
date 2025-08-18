5c96a80ebd7f1012a51268def6428757a569787e
Hari Prasath R <hariprasath.bc004@gmail.com>
2025-08-18 10:07:47 +0530

    Time: 50 ms (41.07%) | Memory: 55.7 MB (52.68%) - LeetSync

---
+++ 1938-minimum-operations-to-make-the-array-increasing/minimum-operations-to-make-the-array-increasing.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var minOperations = function(nums) {
+let count = 0
+for(let i=1 ; i<nums.length ; i++){
+    if(nums[i] <= nums[i-1]){
+        let diff = nums[i-1]-nums[i]+1
+        nums[i] += diff
+    count+=diff
+    }
+}
+return count
+};
