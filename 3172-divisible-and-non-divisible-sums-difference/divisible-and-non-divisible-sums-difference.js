f5dbb809203973e16273d66ca0b9439f27656e9d
Hari Prasath R <hariprasath.bc004@gmail.com>
2025-09-11 09:23:34 +0530

    Time: 1 ms (38.32%) | Memory: 55.4 MB (28.22%) - LeetSync

---
+++ 3172-divisible-and-non-divisible-sums-difference/divisible-and-non-divisible-sums-difference.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number} n
+ * @param {number} m
+ * @return {number}
+ */
+var differenceOfSums = function(n, m) {
+    sum = 0
+Nsum = 0
+for(let i=0;i<=n;i++){
+    if(i%m !== 0){
+      Nsum+=i  
+    }else{
+        sum+=i
+    }
+}return Nsum - sum
+};
