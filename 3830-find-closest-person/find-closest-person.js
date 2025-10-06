1757bd825fcefe5d74aadd8787a28b9b211b7c88
Hari Prasath R <hariprasath.bc004@gmail.com>
2025-10-06 10:52:19 +0530

    Time: 0 ms (100.00%) | Memory: 55.8 MB (20.45%) - LeetSync

--- 3830-find-closest-person/find-closest-person.js
+++ 3830-find-closest-person/find-closest-person.js
@@ -5,18 +5,8 @@
  * @return {number}
  */
 var findClosest = function(x, y, z) {
-    let sma = 0
-let sma1 = 0
-if(x>z){
-    sma = x-z
-}else{
-    sma = z-x
-}
-if(y>z){
-    sma1 = y-z
-}else{
-    sma1 = z-y
-}
+    let sma = Math.abs(x-z)
+let sma1 = Math.abs(y-z)
 let res = 0
 if(sma>sma1){
     res = 2
